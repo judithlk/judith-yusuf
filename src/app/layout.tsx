301653f6bb1729f6c33efe07d3ebd2646c1daa8e
Judith <104207025+judithlk@users.noreply.github.com>
2024-06-24 00:15:08 +0100

    meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,16 @@ const lato = Lato({ subsets: ["latin"], weight: ["400"] });
 export const metadata: Metadata = {
   title: "Welcome :: Judith Yusuf",
   description: "I'm Judith. Welcome to my little corner of the tech space.",
+  openGraph: {
+    title: "Welcome :: Judith Yusuf",
+    description: "I'm Judith. Welcome to my little corner of the tech space.",
+    url: "https://judithyusuf.vercel.app",
+    siteName: "Judith Yusuf - Web Developer",
+    images: [{ url: "https://judithyusuf.vercel.app/og.png",
+      alt: "Judith Yusuf"
+     }],
+     type: "website"
+  },
 };
 
 export default function RootLayout({
@@ -20,6 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
       <ThemeProvider>
         <body className={`dark:bg-dark-bg ${lato.className}`}>
           <NavBar />
